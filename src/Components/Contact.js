b15530e9a870c1b65fa9cb5887e1013d56846e6a
Ramachetan <ramachetan22@gmail.com>
2022-06-15 12:05:06 -0700

    coomit-3

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import { TwitterTimelineEmbed, TwitterFollowButton} from 'react-twitter-embed';
 
 class Contact extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
-      var email = this.props.data.email;
+      var email= this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -50,11 +48,11 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Address, Phone and e-mail</h4>
 					   <p className="address">
 						   {name}<br />
-						   {city}, {state}<br />
-						   <span>{phone}</span>
+						   {city}{state}<br />
+						   <span>{phone}<br /> {email}</span>
 					   </p>
 				   </div>
 
@@ -63,7 +61,7 @@ class Contact extends Component {
                   <ul id="twitter">
                      <li>
                         <span>
-                        Have an idea for a new project? Let's work together to make something cool. Let's learn from each other and make something that will make this world a better place.
+                        Have an idea for a new project? Let's work together to make something. Let's learn from each other and make something to make this world a better place.         
                         <br />
                         <a href = "mailto: ramachetan22@gmail.com">Send Email</a>
                         </span>
